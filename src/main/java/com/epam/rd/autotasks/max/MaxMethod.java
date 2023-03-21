d7f02fa6808913ccb5b0e0c5fcec916da2206695
yuliakordunova97 <kordunovayulia@gmail.com>
2023-03-21 12:34:36 +0200

    added files

---
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -0,0 +1,17 @@
+package com.epam.rd.autotasks.max;
+
+public class MaxMethod {
+    public static void main(String[] args) {
+
+        int[] vals = new int[]{ -2, 0, 10, 5, 2147483620,  15, -70 };
+        int result = MaxMethod.max(vals);
+        System.out.println(result);
+
+    }
+
+    public static int max(int[] values) {
+
+        return 0;
+    }
+
+}
